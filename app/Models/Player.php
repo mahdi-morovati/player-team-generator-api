d2a5834a4ac95fc885377b5abf1843283667c2b3
mahdi <morovati155@gmail.com>
2022-08-06 15:46:45 +0430

    fix Player skill

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -1,7 +1,7 @@
 <?php
 
 // /////////////////////////////////////////////////////////////////////////////
-// PLEASE DO NOT RENAME OR REMOVE ANY OF THE CODE BELOW. 
+// PLEASE DO NOT RENAME OR REMOVE ANY OF THE CODE BELOW.
 // YOU CAN ADD YOUR CODE TO THIS FILE TO EXTEND THE FEATURES TO USE THEM IN YOUR WORK.
 // /////////////////////////////////////////////////////////////////////////////
 
@@ -10,6 +10,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Player extends Model
 {
@@ -19,4 +20,9 @@ class Player extends Model
         'name',
         'position'
     ];
+
+    public function playerSkills(): HasMany
+    {
+        return $this->hasMany(PlayerSkill::class);
+    }
 }
--- database/migrations/2022_05_13_013837_create_player_skills_table.php
+++ database/migrations/2022_05_13_013837_create_player_skills_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('player_skills', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('player_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->enum('skill', ['defense', 'attack', 'speed', 'strength', 'stamina']);
+            $table->string('value');
+            $table->timestamps();
         });
     }
 
