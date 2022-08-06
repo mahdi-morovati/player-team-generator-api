5c89e34dc75c7c2b067a7297485c0ff4acbef34a
mahdi <morovati155@gmail.com>
2022-08-06 14:40:13 +0430

    add players columns

--- database/migrations/2022_04_15_180941_create_players_table.php
+++ database/migrations/2022_04_15_180941_create_players_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('players', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->enum('position', ['defender', 'midfielder', 'forward']);
+            $table->timestamps();
         });
     }
 
