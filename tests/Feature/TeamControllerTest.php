1fa222a08ae84457a931f2e25074c5490f8ec8bf
mahdi <morovati155@gmail.com>
2022-08-07 16:22:13 +0430

    create SelectTeamController.php, SelectTeamRequest.php

--- tests/Feature/TeamControllerTest.php
+++ tests/Feature/TeamControllerTest.php
@@ -12,15 +12,33 @@ class TeamControllerTest extends PlayerControllerBaseTest
 {
     public function test_sample()
     {
-        $requirements =
+//        $requirements =
+//            [
+//                'position' => "defender",
+//                'mainSkill' => "speed",
+//                'numberOfPlayers' => 1
+//            ];
+
+        $requirements = [
             [
                 'position' => "defender",
                 'mainSkill' => "speed",
                 'numberOfPlayers' => 1
-            ];
+            ],
+//            [
+//                'position' => "defender",
+//                'mainSkill' => "defense",
+//                'numberOfPlayers' => 1
+//            ],
+            [
+                'position' => "midfielder",
+                'mainSkill' => "speed",
+                'numberOfPlayers' => 1
+            ]
+        ];
 
 
-        $res = $this->postJson(self::REQ_TEAM_URI, $requirements);
+        $res = $this->postJson(self::REQ_TEAM_URI, ['data' => $requirements]);
 
         $this->assertNotNull($res);
     }
